5ab23f1aaae62d43177e2300e2e00f4d94d1e7af
MugureNjunge <maureennjunge22@gmail.com>
2022-06-04 15:04:05 +0300

    create profile class with all properties

---
+++ instagram/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from instagram import views
+from views import EditProfile, UserProfile
+
+# urlpatterns = [
+    # Profile Section
+#     path('profile/edit', EditProfile, name="editprofile"),
+
+#     # User Authentication
+#     path('sign-up/', views.register, name="sign-up"),
+#     path('sign-in/', views.Login, template_name="sign-in.html", redirect_authenticated_user=True , name='sign-in'),
+#     path('sign-out/', views.Logout, template_name="sign-out.html", name='sign-out'), 
+# ]
+
--- instapics/urls.py
+++ instapics/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from photobomb import views
+from django.urls import path, include
+from instagram import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name="homepage")
+    path('',include('instagram.urls'))
 ]
