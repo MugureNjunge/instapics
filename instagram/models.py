7e8dc80cefe1b9419128483667447384718c70d0
MugureNjunge <maureennjunge22@gmail.com>
2022-06-04 15:14:22 +0300

    create tag and post models

--- instagram/models.py
+++ instagram/models.py
@@ -11,13 +11,12 @@ from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="profile_pciture", null=True, default="default.jpg")
+    image = models.ImageField(upload_to="profile_picture", null=True, default="default.jpg")
     first_name = models.CharField(max_length=200, null=True, blank=True)
     last_name = models.CharField(max_length=200, null=True, blank=True)
     bio = models.CharField(max_length=200, null=True, blank=True)
     location = models.CharField(max_length=200, null=True, blank=True)
     url = models.URLField(max_length=200, null=True, blank=True)
-    # favourite = models.ManyToManyField(Post, blank=True)
 
     
     def save(self, *args, **kwargs):
@@ -28,12 +27,6 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        
-        # img = Image.open(self.image.path)
-        # if img.height > 300 or img.width > 300:
-        #     output_size = (300, 300)
-        #     img.thumbnail(output_size)
-        #     img.save(self.image.path)
 
 
     def create_user_profile(sender, instance, created, **kwargs):
@@ -45,3 +38,26 @@ class Profile(models.Model):
 
     post_save.connect(create_user_profile, sender=User)
     post_save.connect(save_user_profile, sender=User)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=75, verbose_name='Tag')
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def __str__(self):
+        return self.title    
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    squareImage = models.ImageField(upload_to="new_post", null=True, default="default.jpg")
+    caption = models.CharField(max_length=10000, verbose_name="Caption")
+    posted = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="tags")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+
+
+
+     
