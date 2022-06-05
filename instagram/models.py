f66d68b1e1961e5acb64eccdf1bd3611859136e8
MugureNjunge <maureennjunge22@gmail.com>
2022-06-05 16:48:21 +0300

    add followers count model and migrate

--- instagram/models.py
+++ instagram/models.py
@@ -1,183 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 import uuid
-from django.utils import timezone
-from django.db.models.signals import post_save, post_delete
-from django.db.models import Max
-# from django.db.models.base import Model
-# from django.db.models.fields import DateField
-# from django.urls import reverse
+from datetime import datetime
 
+User = get_user_model()
 
-from django.forms import DateTimeField
-
-
+# Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="profile_picture", null=True, default="default.jpg")
-    first_name = models.CharField(max_length=200, null=True, blank=True)
-    last_name = models.CharField(max_length=200, null=True, blank=True)
-    bio = models.CharField(max_length=200, null=True, blank=True)
-    location = models.CharField(max_length=200, null=True, blank=True)
-    url = models.URLField(max_length=200, null=True, blank=True)
-
-    
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return f'{self.user.username} - Profile'
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-
-    def create_user_profile(sender, instance, created, **kwargs):
-      if created:
-        Profile.objects.create(user=instance)
-
-    def save_user_profile(sender, instance, **kwargs):
-      instance.profile.save()
-
-    post_save.connect(create_user_profile, sender=User)
-    post_save.connect(save_user_profile, sender=User)
-
-class Tag(models.Model):
-    title = models.CharField(max_length=75, verbose_name='Tag')
-
-    class Meta:
-        verbose_name = 'Tag'
-        verbose_name_plural = 'Tags'
-
-    def __str__(self):
-        return self.title    
+        return self.user.username
 
 class Post(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    squareImage = models.ImageField(upload_to="new_post", null=True, default="default.jpg")
-    caption = models.CharField(max_length=10000, verbose_name="Caption")
-    posted = models.DateField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag, related_name="tags")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.IntegerField(default=0)
-
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True, null=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.post
-    
-    def user_comment_post(sender, instance, *args, **kwargs):
-        comment = instance
-        post = comment.post
-        text_preview = comment.body[:90]
-        sender = comment.user
-        notify = Notification(post=post, sender=sender, user=post.user, text_preview=text_preview, notification_types=2)
-        notify.save()
+        return self.user
 
-    def user_del_comment_post(sender, instance, *args, **kwargs):
-        comment = instance
-        post = comment.post
-        sender = comment.user
-        notify = Notification.objects.filter(post=post, sender=sender, user=post.user, notification_types=2)
-        notify.delete()
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
 
-post_save.connect(Comment.user_comment_post, sender=Comment)
-post_delete.connect(Comment.user_del_comment_post, sender=Comment)
+    def __str__(self):
+        return self.username
 
-class Notification(models.Model):
-    NOTIFICATION_TYPES = ((1, 'Like'), (2, 'Comment'), (3, 'Follow'))
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
 
-    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="notification_post", null=True)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notification_from_user" )
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notification_to_user" )
-    notification_types = models.IntegerField(choices=NOTIFICATION_TYPES, null=True, blank=True)
-    text_preview = models.CharField(max_length=100, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    is_seen = models.BooleanField(default=False)
-
-class Follow(models.Model):
-  pass    
-
-class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="from_user")
-    reciepient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="to_user")
-    body = models.TextField(null=True)
-    date = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
-
-    def sender_message(from_user, to_user, body):
-        sender_message = Message(
-            user=from_user,
-            sender = from_user,
-            reciepient = to_user,
-            body = body,
-            is_read = True
-            )
-        sender_message.save()
-    
-        reciepient_message = Message(
-            user=to_user,
-            sender = from_user,
-            reciepient = from_user,
-            body = body,
-            is_read = True
-            )
-        reciepient_message.save()
-        return sender_message
-
-    def get_message(user):
-        users = []
-        messages = Message.objects.filter(user=user).values('reciepient').annotate(last=Max('date')).order_by('-last')
-        for message in messages:
-            users.append({
-                'user': User.objects.get(pk=message['reciepient']),
-                'last': message['last'],
-                'unread': Message.objects.filter(user=user, reciepient__pk=message['reciepient'], is_read=False).count()
-            })
-        return users
-
-class Likes(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_likes")
-
-    def user_liked_post(sender, instance, *args, **kwargs):
-        like = instance
-        post = like.post
-        sender = like.user
-        notify = Notification(post=post, sender=sender, user=post.user)
-        notify.save()
-
-    def user_unliked_post(sender, instance, *args, **kwargs):
-        like = instance
-        post = like.post
-        sender = like.user
-        notify = Notification.objects.filter(post=post, sender=sender, notification_types=1)
-        notify.delete()
-
-
-class Follow(models.Model):
-    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
-
-    def user_follow(sender, instance, *args, **kwargs):
-        follow = instance
-        sender = follow.follower
-        following = follow.following
-        notify = Notification(sender=sender, user=following, notification_types=3)
-        notify.save()
-
-    def user_unfollow(sender, instance, *args, **kwargs):
-        follow = instance
-        sender = follow.follower
-        following = follow.following
-        notify = Notification.objects.filter(sender=sender, user=following, notification_types=3)
-        notify.delete()        
-
-     
+    def __str__(self):
+        return self.user
