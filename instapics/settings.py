7fa6323fe4c2fdb0c6d62992217149b76cd74d7c
MugureNjunge <maureennjunge22@gmail.com>
2022-06-07 08:09:45 +0300

    add images to static folder

--- instapics/settings.py
+++ instapics/settings.py
@@ -140,13 +140,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = ''
 
-LOGOUT_REDIRECT_URL = 'sign-in'
+LOGOUT_REDIRECT_URL = ''
 
-LOGIN_URL = 'sign-in'
+LOGIN_URL = ''
 
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
