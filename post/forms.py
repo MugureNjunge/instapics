439499d907dc557ec61ac25fa3cf419691ae08d3
MugureNjunge <maureennjunge22@gmail.com>
2022-06-07 08:20:12 +0300

    create a newpost html file as well as the view method

--- post/forms.py
+++ post/forms.py
@@ -3,12 +3,11 @@ from post.models import Post
 
 
 class NewPostform(forms.ModelForm):
-    # content = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=True)
-    
+   
     picture = forms.ImageField(required=True)
     caption = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Caption'}), required=True)
-    tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Tags | Seperate with comma'}), required=True)
+    tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Tags'}), required=True)
 
-    # class Meta:
-    #     model = Post
-    #     fields = ['picture', 'caption', 'tags']
+    class Meta:
+        model = Post
+        fields = ['picture', 'caption', 'tags']
