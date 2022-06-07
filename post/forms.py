c1931ea38f1cb2ba051548f048769651f8ef0024
MugureNjunge <maureennjunge22@gmail.com>
2022-06-07 08:15:03 +0300

    create forms.py in my post app to allow users add posts

--- post/forms.py
+++ post/forms.py
@@ -9,6 +9,6 @@ class NewPostform(forms.ModelForm):
     caption = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Caption'}), required=True)
     tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Tags | Seperate with comma'}), required=True)
 
-    class Meta:
-        model = Post
-        fields = ['picture', 'caption', 'tags']
+    # class Meta:
+    #     model = Post
+    #     fields = ['picture', 'caption', 'tags']
