c8ac0182def78d5191856f4db5cec2a1b5c70aa0
MugureNjunge <maureennjunge22@gmail.com>
2022-06-07 17:53:46 +0300

    write tests for my message and comment classes

--- comment/tests.py
+++ comment/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='shiru')
+
+        self.comment= Comment.objects.create(poster= self.user, comment='looking good' )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment, Comment))
+
+    def test_save_comment(self):
+        self.assertTrue(isinstance(self.comment,Comment))
+
+    def test_get_comment(self):
+        self.comment.save()
+        comment = Comment.get_comment()
+        self.assertTrue(len(comment) == 1)
+
--- directs/tests.py
+++ directs/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class MessageTestClass(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='shiru')
+
+        self.message= Message.objects.create(poster= self.user, comment='Thanks for the dm' )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.message, Message))
+
+    def test_save_message(self):
+        self.assertTrue(isinstance(self.message,Message))
+
+    def test_get_message(self):
+        self.message.save()
+        message = Message.get_message()
+        self.assertTrue(len(message) == 1)
+
+
