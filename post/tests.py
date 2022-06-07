30fbfac16a1f05e9b5aa95900962c1cff8b96640
MugureNjunge <maureennjunge22@gmail.com>
2022-06-07 18:04:03 +0300

    write tests for my image/post class

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import *
+from instagram.models import Profile
 
-# Create your tests here.
+class PostTestClass(TestCase):
+  
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='shiru')
+        self.profile = Profile.objects.create(user = self.user,bio = 'forever young')
+
+        self.image = Post.objects.create(name = self.user,profile = self.profile,caption ='lets be forever young',likes = 0,posted='7/06/2022')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.picture,Post))
+
+    def test_get_pictures(self):
+        self.image.save()
+        picture = Post.get_pictures()
+        self.assertTrue(len(picture) == 1)
+
+
